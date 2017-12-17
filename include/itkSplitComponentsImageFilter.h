0fcfe97c02495b6d08544363c7fa101cf61a0f8a
Hans Johnson <hans-johnson@uiowa.edu>
2017-12-16 19:36:27 -0600

    COMP:  Use C++11 override directly
    git grep -l "ITK_OVERRIDE" |   fgrep -v itk_compiler_detection.h | fgrep -v CMakeLists.txt |fgrep -v .cmake |   xargs sed -i '' -e "s/ITK_OVERRIDE/override/g"

--- include/itkSplitComponentsImageFilter.h
+++ include/itkSplitComponentsImageFilter.h
@@ -85,12 +85,12 @@ public:
 
 protected:
   SplitComponentsImageFilter();
-  ~SplitComponentsImageFilter() ITK_OVERRIDE {}
+  ~SplitComponentsImageFilter() override {}
 
   /** Do not allocate outputs that we will not populate. */
-  void AllocateOutputs() ITK_OVERRIDE;
+  void AllocateOutputs() override;
 
-  void ThreadedGenerateData( const OutputRegionType& outputRegion, ThreadIdType threadId ) ITK_OVERRIDE;
+  void ThreadedGenerateData( const OutputRegionType& outputRegion, ThreadIdType threadId ) override;
 
 private:
   ITK_DISALLOW_COPY_AND_ASSIGN(SplitComponentsImageFilter);
