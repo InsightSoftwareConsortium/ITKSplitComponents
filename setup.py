bbc62b1e8b203771c2588a59fb648b79557d2cbc
Tom Birdsong <tom.birdsong@kitware.com>
2022-04-07 15:54:44 -0400

    ENH: Bump ITK to v5.3rc04

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ except ImportError:
 
 setup(
     name='itk-splitcomponents',
-    version='2.0.5',
+    version='2.0.6',
     author='Matthew M. McCormick',
     author_email='matt.mccormick@kitware.com',
     packages=['itk'],
@@ -50,6 +50,6 @@ setup(
     keywords='ITK Higher-order Derivative Gradient',
     url=r'https://github.com/InsightSoftwareConsortium/ITKSplitComponents',
     install_requires=[
-        r'itk>=5.2.0.post2'
+        r'itk>=5.3rc04'
     ]
     )
